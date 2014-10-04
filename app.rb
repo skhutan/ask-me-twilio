0278c7907f0adb4ac6f29cfd26392e36a9dae953
Calum Gilchrist <calum@homelinen.org>
2014-10-04 17:47:58 +0100

    Initial Test Implementation

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'twilio-ruby'
+require 'yaml'
+
+get '/' do
+
+    'Hi'
+end
+
+get '/send_sms/:name' do |name|
+
+    config = YAML.load_file('config.yaml')
+    account_sid=config['account']['account_sid']
+    auth_token=config['account']['auth_token']
+
+    to_number=config['to']
+    from_number=config['from']
+
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    client.account.messages.create(
+        :to => to_number,
+        from: from_number,
+        body: "Hello #{name}"
+    )
+
+    'You sent a message'
+end
