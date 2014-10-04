e084474503f28b718a80849333a83af617e61799
Calum Gilchrist <calum@homelinen.org>
2014-10-04 20:43:53 +0100

    Added a Request Route for Testing

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require 'sinatra'
 require 'twilio-ruby'
 require 'yaml'
 
-get '/' do
+require './lib/command_parse'
 
+get '/' do
     'Hi'
 end
 
@@ -25,4 +26,24 @@ get '/send_sms/:name' do |name|
     )
 
     'You sent a message'
+end
+
+# Eat the post request from twilio
+# FIXME: Need a way to lock this down? User-agent?
+post '/twil/received' do
+    
+    message = params['Body'].strip
+
+    parsed = Parser.parse(message)
+
+    command = parsed[:command]
+    args = parsed[:args]
+    
+    case command
+        when 'weather'
+            puts 'the weather is nice'
+        else
+            puts "Command #{command} not found"
+    end
+    'ok'
 end
