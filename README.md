721cd755ea9b98f8c1e29920e8792e7348fedb83
Sunil Khutan <sdk4@hw.ac.uk>
2014-10-04 22:12:54 +0100

    add because calum said

--- README.md
+++
@@ -1,4 +0,0 @@
-ask-me-twilio
-=============
-
-Hacking with Twilio API at MLH London
--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'twilio-ruby'
+require 'wolfram'
 require 'yaml'
+
+config = YAML.load_file('config.yaml')
+account_sid=config['account']['account_sid']
+auth_token=config['account']['auth_token']
+to_number=config['to']
+from_number=config['from']
 
 get '/' do
 
@@ -9,20 +16,24 @@ end
 
 get '/send_sms/:name' do |name|
 
-    config = YAML.load_file('config.yaml')
-    account_sid=config['account']['account_sid']
-    auth_token=config['account']['auth_token']
+  client = Twilio::REST::Client.new account_sid, auth_token
 
-    to_number=config['to']
-    from_number=config['from']
+  client.account.messages.create(
+      :to => to_number,
+      from: from_number,
+      body: "Hello #{name}"
+  )
 
-    client = Twilio::REST::Client.new account_sid, auth_token
+  'You sent a message'
+end
 
-    client.account.messages.create(
-        :to => to_number,
-        from: from_number,
-        body: "Hello #{name}"
-    )
+get '/wolfram/:query' do |query|
+  get_wolfram(query)
+end
 
-    'You sent a message'
+def get_wolfram(query)
+  result = Wolfram.fetch(query)
+  # to see the result as a hash of pods and assumptions:
+  hash = Wolfram::HashPresenter.new(result).to_hash
+  hash[:pods]["Result"]
 end
