b8ef4e88bf5c4f41f0e82e244ca6c078d65fb932
Sunil Khutan <sdk4@hw.ac.uk>
2014-10-05 01:30:37 +0100

    added voice call demo

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'twilio-ruby'
 require 'wolfram'
 require 'yaml'
-
 require './lib/command_parse'
 
 config = YAML.load_file('config.yaml')
@@ -10,14 +9,33 @@ config = YAML.load_file('config.yaml')
 @@auth_token=config['account']['auth_token']
 @@to_number=config['to']
 @@from_number=config['from']
+@@client = Twilio::REST::Client.new @@account_sid, @@auth_token
 
 get '/' do
-  'Hi'
+  "Hi"
+end
+
+get '/voicemail' do
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say 'You have one MESSAGE... BEEP!!!', voice: 'woman'
+    r.Dial callerId: @@to_number do |d|
+      d.Client 'Calum'
+    end
+  end
+end
+
+get '/callme' do
+  @call = @@client.account.calls.create(
+  from: @@from_number, # From your Twilio number
+  to: @@to_number, # To any number
+  # Fetch instructions from this URL when the call connects
+  url: 'http://mlh.homelinen.org/voicemail'
+)
 end
 
 get '/send_sms/:name' do |name|
   send_sms("Hello #{name}", @@to_number)
-  %{You sent a message to #{@@to_number}}
+  %{You sent a message to #{ @@to_number }}
 end
 
 get '/wolfram/:query' do |query|
@@ -27,27 +45,26 @@ end
 # Eat the post request from twilio
 # FIXME: Need a way to lock this down? User-agent?
 post '/twil/received' do
-    message = params['Body'].strip
-    from = params["From"]
-    parsed = Parser.parse(message)
+  message = params['Body'].strip
+  from = params["From"]
+  parsed = Parser.parse(message)
 
-    command = parsed[:command]
-    args = parsed[:args]
-    
-    case command
-        when 'weather'
-          puts 'the weather is nice'
-        when 'wolfram'
-          wolfram_result = get_wolfram(args.join())
-          send_sms(wolfram_result, from)
-        else
-          puts "Command #{command} not found"
-    end
-    'ok'
+  command = parsed[:command]
+  args = parsed[:args]
+  
+  case command
+      when 'weather'
+        puts 'the weather is nice'
+      when 'wolfram'
+        wolfram_result = get_wolfram(args.join())
+        send_sms(wolfram_result, from)
+      else
+        puts "Command #{command} not found"
+  end
+  'ok'
 end
 
 def send_sms(message, phone_number)
-  @@client = Twilio::REST::Client.new @@account_sid, @@auth_token
   @@client.account.messages.create(
     to: phone_number,
     from: @@from_number,
